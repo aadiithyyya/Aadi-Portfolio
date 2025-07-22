15544eae499621ef8894ca15b41ae4b21327e8db
Aadithya Ram <aadithyaram@Aadithyas-MacBook-Air.local>
2025-07-23 00:59:49 +0530

    next

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Scan all components/pages
+    "./public/index.html"          // Scan HTML (optional)
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
